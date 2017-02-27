884fe091efebca04a4cb84a9f4b7fb2924dd7a2b
Nkr1shna <kr1shna@outlook.com>
2017-02-27 12:39:23 -0500

    Pushed the Django Project to git

---
+++ plalyst/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from login.views import *
+from django.contrib.auth.views import login
+
+urlpatterns = [
+    # the ones related to the login app
+    url(r'^$', include('login')),
+    url(r'^logout/$', logout_page),
+    url(r'^accounts/login/$', include('login')),  # If user is not login it will redirect to login page
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+    url(r'^home/$', home),
+    url(r'^admin/', include(admin.site.urls)),
+]
