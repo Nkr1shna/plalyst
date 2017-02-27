058d47cb8ce522277a780d4649337508a2d088e1
manmithak <manmitha.ck@gmail.com>
2017-02-27 13:14:42 -0500

    Created song model

--- song/models.py
+++ song/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    year = models.CharField(max_length=200)
+
+# Check song exists methods
+# - checks if the songs is present in the database
+# -if present it allows the user to add the next song
