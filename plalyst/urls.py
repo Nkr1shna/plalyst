bad6c592fbce5d9cdcbcabd2a3e022bba5aca502
Nkr1shna <kr1shna@outlook.com>
2017-03-01 18:17:19 -0500

    Added the login page

--- plalyst/urls.py
+++ plalyst/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from login.views import *
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import login
 
 urlpatterns = [
     # the ones related to the login app
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
     url(r'^youtube/', include('youtubeplayer.urls')),
 ]
