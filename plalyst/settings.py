2c29041894fcf09c89af35957bfbce40d15e7b8d
Nkr1shna <kr1shna@outlook.com>
2017-03-27 00:42:26 -0400

    Added local Musicbrainz database access

--- plalyst/settings.py
+++ plalyst/settings.py
@@ -83,6 +83,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'musicbrainz': {
+        'NAME': 'musicbrainz',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'musicbrainz',
+        'PASSWORD': 'musicbrainz',
+        'HOST': 'localhost',
     }
 }
 
