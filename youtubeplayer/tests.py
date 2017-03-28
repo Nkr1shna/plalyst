7bfe065ba9ba687fd4f885c849e556ce58817f26
manmithak <manmitha.ck@gmail.com>
2017-03-27 20:05:47 -0400

    Entered test cases

--- youtubeplayer/tests.py
+++ youtubeplayer/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ExampleTestCase(APITestCase):
+    def test_url_root(self):
+        url = "https://www.youtube.com/watch?v=WWa4bzTJZgI"
+        response = self.client.get(url)
+        self.assertTrue(status.is_success(response.status_code))
