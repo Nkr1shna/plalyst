9fb2ce8ac86000f778cd3bba712c316ddb5ce14c
krishna <kr1shna@outlook.com>
2017-04-29 17:53:44 -0400

    Tests edited

--- login/manmithaTests.py
+++ login/manmithaTests.py
@@ -20,7 +20,7 @@ class CreatePlaylist(unittest.TestCase):
         driver = self.driver
         driver.get(self.base_url + "login/")
         driver.find_element_by_id("id_username").clear()
-        driver.find_element_by_id("id_username").send_keys(user.username)
+        driver.find_element_by_id("id_username").send_keys(user.name)
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys(user.password)
         driver.find_element_by_css_selector("button.btn.btn-success").click()
@@ -79,7 +79,7 @@ class DeletePlaylist(unittest.TestCase):
         user = LoginData()
         driver.get(self.base_url + "/login/logout_user/")
         driver.find_element_by_id("id_username").clear()
-        driver.find_element_by_id("id_username").send_keys(user.username)
+        driver.find_element_by_id("id_username").send_keys(user.name)
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys(user.password)
         driver.find_element_by_css_selector("button.btn.btn-success").click()
@@ -136,7 +136,7 @@ class Logout2(unittest.TestCase):
         user = LoginData()
         driver.get(self.base_url + "login/")
         driver.find_element_by_id("id_username").clear()
-        driver.find_element_by_id("id_username").send_keys(user.username)
+        driver.find_element_by_id("id_username").send_keys(user.name)
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys(user.password)
         driver.find_element_by_css_selector("button.btn.btn-success").click()
@@ -217,6 +217,123 @@ class Register(unittest.TestCase):
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys(user.password)
         driver.find_element_by_css_selector("button.btn.btn-success").click()
+        driver.find_element_by_link_text("Logout").click()
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to.alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to.alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+class Login(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost:8000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_login(self):
+        driver = self.driver
+        user = LoginData()
+        driver.get(self.base_url + "login/")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys(user.name)
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys(user.password)
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        driver.find_element_by_link_text("Logout").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.swtich_to.alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to.alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+class DeleteSong(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost:8000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_delete_song(self):
+        songList = inputSong()
+        driver = self.driver
+        user = LoginData()
+        driver.get(self.base_url + "login/")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys(user.name)
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys(user.password)
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        driver.find_element_by_link_text("Add Plalyst").click()
+        driver.find_element_by_id("id_Plalyst_title").clear()
+        driver.find_element_by_id("id_Plalyst_title").send_keys(PlaylistName())
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        driver.find_element_by_link_text("Add New Song").click()
+        driver.find_element_by_id("id_song_title").clear()
+        driver.find_element_by_id("id_song_title").send_keys(songList[0])
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        driver.find_element_by_link_text("View All").click()
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        driver.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
         try:
