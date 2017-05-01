fadffc4642d4da353d93041ad3d603b6a4b3849f
Ankiredd <gouthamireddy111@gmail.com>
2017-05-01 13:21:42 -0400

    Update locust_file.py

--- login/locust_file.py
+++ login/locust_file.py
@@ -1,4 +1,3 @@
-# locustfile.py
 from locust import HttpLocust, TaskSet, task
 
 
@@ -6,6 +5,7 @@ class UserBehavior(TaskSet):
 
     def on_start(self):
         self.login()
+
 
     def login(self):
         # GET login page to get csrftoken from it
@@ -16,9 +16,18 @@ class UserBehavior(TaskSet):
                          {'username': 'gouthu123', 'password': 'gouthu123'},
                          headers={'X-CSRFToken': csrftoken})
 
-    @task(1)
+    @task
     def index(self):
         self.client.get('/')
+
+    @task
+    def register(self):
+        response = self.client.get('http://localhost:8000/register/')
+        csrftoken = response.cookies['csrftoken']
+
+        self.client.post('http://localhost:8000/register/',
+                         {'username':'achyuth','email':'achyut@gmail.com','password':'achyuth'},
+                         headers={'X-CSRFToken': csrftoken})
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
