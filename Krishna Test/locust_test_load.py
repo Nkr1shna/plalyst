ddb20b34e93a19772d370de50616c2f21754870a
krishna <kr1shna@outlook,com>
2017-05-01 12:50:44 -0400

    Loadtest Added

---
+++ Krishna Test/locust_test_load.py
@@ -0,0 +1,23 @@
+from locust import HttpLocust, TaskSet
+
+def login(l):
+    response = l.client.get('http://localhost:8000/login/')
+    csrftoken = response.cookies['csrftoken']
+    l.client.post('http://localhost:8000/login/',
+                     {'username': 'kr1shna', 'password': '40OZlike'},
+                     headers={'X-CSRFToken': csrftoken})
+
+def gp(l):
+    l.client.get("/generate/1")
+
+
+class UserBehavior(TaskSet):
+    tasks = {login: 2, gp:2}
+
+    def on_start(self):
+        login(self)
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
